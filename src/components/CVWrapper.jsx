522ad0d4a7c8e688c116d4b712b3964c1b2e99d6
Fabius Gasber <fabiusgasber@gmail.com>
2025-04-11 18:02:12 +0200

    refactor(CVWrapper): simplify markup and fix missing key warning

--- src/components/CVWrapper.jsx
+++ src/components/CVWrapper.jsx
@@ -3,13 +3,11 @@ import "../styles/cv.css"
 const CVWrapper = ({ data, title }) => {
     return (
     <div className="cv-wrapper">
-        {title ? <div><h2>{title}</h2></div> : ""}
+        {title && <div><h2>{title}</h2></div>}
         {data.map(form => 
-        <>
            <ul className={form.form} key={form.id}>{Object.entries(form).map(([key, value], index) => 
-            key !== "id" && key !== "form" ? <li key={index} className={key}>{value}</li> : ""
+            key !== "id" && key !== "form" ? <li key={index} className={key}>{value}</li> : null
            )}</ul>
-        </>
         )}
     </div>
     )
