eb7801bc9809634e20588bc5807d5d586a13cef5
Fabius Gasber <fabiusgasber@gmail.com>
2025-04-15 17:38:16 +0200

    feat: add a11y friendly keyboard support for cv preview

--- src/components/CVWrapper.jsx
+++ src/components/CVWrapper.jsx
@@ -2,11 +2,11 @@ import "../styles/cv.css"
 
 const CVWrapper = ({ data, title }) => {
     return (
-    <div className="cv-wrapper">
+    <div className="cv-wrapper" aria-label={title ? title + " cv preview" : "personal info cv preview" } tabIndex={0}>
         {title && <div><h2>{title}</h2></div>}
         {data.map(form => 
-           <ul className={form.form} key={form.id}>{Object.entries(form).map(([key, value], index) => 
-            key !== "id" && key !== "form" ? <li key={index} className={key}>{value}</li> : null
+           <ul className={form.form} key={form.id} tabIndex={0}>{Object.entries(form).map(([key, value], index) => 
+            key !== "id" && key !== "form" ? <li tabIndex={0} key={index} className={key}>{value}</li> : null
            )}</ul>
         )}
     </div>
